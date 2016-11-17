445beef1cc9fa22e813d82319796b53a0e2f660d
Jackal <ywzjackal@163.com>
2016-11-17 14:27:36 +0800

    change adjust_os_time

--- src/linux.rs
+++ src/linux.rs
@@ -16,27 +16,27 @@ const SCHED_FIFO: i32 = 1;
 const SCHED_RR: i32 = 2;
 
 pub const CLOCK_REALTIME: i32 = 0;
-/* Monotonic system-wide clock.  */
+// Monotonic system-wide clock.
 pub const CLOCK_MONOTONIC: i32 = 1;
-/* High-resolution timer from the CPU.  */
+// High-resolution timer from the CPU.
 const CLOCK_PROCESS_CPUTIME_ID: i32 = 2;
-/* Thread-specific CPU-time clock.  */
+// Thread-specific CPU-time clock.
 const CLOCK_THREAD_CPUTIME_ID: i32 = 3;
-/* Monotonic system-wide clock, not adjusted for frequency scaling.  */
+// Monotonic system-wide clock, not adjusted for frequency scaling.
 const CLOCK_MONOTONIC_RAW: i32 = 4;
-/* Identifier for system-wide realtime clock, updated only on ticks.  */
+// Identifier for system-wide realtime clock, updated only on ticks.
 const CLOCK_REALTIME_COARSE: i32 = 5;
-/* Monotonic system-wide clock, updated only on ticks.  */
+// Monotonic system-wide clock, updated only on ticks.
 const CLOCK_MONOTONIC_COARSE: i32 = 6;
-/* Monotonic system-wide clock that includes time spent in suspension.  */
+// Monotonic system-wide clock that includes time spent in suspension.
 const CLOCK_BOOTTIME: i32 = 7;
-/* Like CLOCK_REALTIME but also wakes suspended system.  */
+// Like CLOCK_REALTIME but also wakes suspended system.
 const CLOCK_REALTIME_ALARM: i32 = 8;
-/* Like CLOCK_BOOTTIME but also wakes suspended system.  */
+// Like CLOCK_BOOTTIME but also wakes suspended system.
 const CLOCK_BOOTTIME_ALARM: i32 = 9;
-/* Like CLOCK_REALTIME but in International Atomic Time.  */
+// Like CLOCK_REALTIME but in International Atomic Time.
 const CLOCK_TAI: i32 = 11;
-/* Flag to indicate time is absolute.  */
+// Flag to indicate time is absolute.
 const TIMER_ABSTIME: i32 = 1;
 
 type TimerId = c_long;
@@ -67,7 +67,8 @@ impl Timer {
         (Timer {
             timer_id: 0,
             tx: tx,
-        }, rx)
+        },
+         rx)
     }
 
     // Setup timer in ticker mode.
@@ -75,9 +76,7 @@ impl Timer {
         let mut pthread_attr = pthread_attr_t::new();
         let timer_id_ptr: *mut TimerId = &mut self.timer_id;
         let mut sigevent = sigevent_t::with_callback(cb, self, policy, &mut pthread_attr).unwrap();
-        if unsafe {
-            timer_create(clock_type, &mut sigevent, timer_id_ptr)
-        } != 0 {
+        if unsafe { timer_create(clock_type, &mut sigevent, timer_id_ptr) } != 0 {
             Err(Error::last_os_error())
         } else {
             Ok(())
@@ -86,10 +85,16 @@ impl Timer {
 
     // Start ticker with TIMER_ABSTIME mode.
     pub fn start_abstime(&self, interval: Duration, from: Duration) -> Result<()> {
-        let interval = timespec_t::new(interval.as_secs() as c_long, interval.subsec_nanos() as c_long);
+        let interval = timespec_t::new(interval.as_secs() as c_long,
+                                       interval.subsec_nanos() as c_long);
         let start = timespec_t::new(from.as_secs() as c_long, from.subsec_nanos() as c_long);
         let mut itime = itimerspec_t::with_value(interval, start);
-        let rt = unsafe { timer_settime(self.timer_id, TIMER_ABSTIME, &mut itime, 0 as *mut itimerspec_t) };
+        let rt = unsafe {
+            timer_settime(self.timer_id,
+                          TIMER_ABSTIME,
+                          &mut itime,
+                          0 as *mut itimerspec_t)
+        };
         if rt != 0 {
             Err(Error::last_os_error())
         } else {
@@ -99,7 +104,8 @@ impl Timer {
 
     // Start ticker with TIMER_RELATIVE mode.
     pub fn start_reltime(&self, interval: Duration, from: Duration) -> Result<()> {
-        let interval = timespec_t::new(interval.as_secs() as c_long, interval.subsec_nanos() as c_long);
+        let interval = timespec_t::new(interval.as_secs() as c_long,
+                                       interval.subsec_nanos() as c_long);
         let start = timespec_t::new(from.as_secs() as c_long, from.subsec_nanos() as c_long);
         let mut itime = itimerspec_t::with_value(interval, start);
         let rt = unsafe { timer_settime(self.timer_id, 0, &mut itime, 0 as *mut itimerspec_t) };
@@ -115,7 +121,12 @@ impl Timer {
         let interval = timespec_t::new(0, 0);
         let start = timespec_t::new(0, 0);
         let mut itime = itimerspec_t::with_value(interval, start);
-        let rt = unsafe { timer_settime(self.timer_id, TIMER_ABSTIME, &mut itime, 0 as *mut itimerspec_t) };
+        let rt = unsafe {
+            timer_settime(self.timer_id,
+                          TIMER_ABSTIME,
+                          &mut itime,
+                          0 as *mut itimerspec_t)
+        };
         if rt != 0 {
             Err(Error::last_os_error())
         } else {
@@ -145,21 +156,15 @@ struct sched_param_t {
 impl sched_param_t {
     fn new(priority: i32) -> sched_param_t {
         let mut priority = priority;
-        let min = unsafe {
-            sched_get_priority_min(SCHED_FIFO)
-        };
-        let max = unsafe {
-            sched_get_priority_max(SCHED_FIFO)
-        };
+        let min = unsafe { sched_get_priority_min(SCHED_FIFO) };
+        let max = unsafe { sched_get_priority_max(SCHED_FIFO) };
         if priority > max {
             priority = max;
         }
         if priority < min {
             priority = min;
         }
-        sched_param_t {
-            sched_priority: priority,
-        }
+        sched_param_t { sched_priority: priority }
     }
 }
 
@@ -179,12 +184,16 @@ struct sigevent_t {
     pub sigev_value: CallbackFunctionParam,
     pub sigev_signo: c_int,
     pub sigev_notify: c_int,
-    pub function: extern fn(CallbackFunctionParam),
+    pub function: extern "C" fn(CallbackFunctionParam),
     pub attribute: *mut pthread_attr_t,
 }
 
 impl sigevent_t {
-    fn with_callback(cb: extern fn(CallbackFunctionParam), param: CallbackFunctionParam, priority: i32, attr: *mut pthread_attr_t) -> Result<sigevent_t> {
+    fn with_callback(cb: extern "C" fn(CallbackFunctionParam),
+                     param: CallbackFunctionParam,
+                     priority: i32,
+                     attr: *mut pthread_attr_t)
+                     -> Result<sigevent_t> {
         let sigevent = sigevent_t {
             sigev_value: param,
             sigev_signo: 0,
@@ -194,7 +203,7 @@ impl sigevent_t {
         };
         let mut sched_param = sched_param_t::new(priority);
         unsafe {
-            
+
             let rt = sched_setscheduler(0, SCHED_FIFO, &mut sched_param);
             if rt != 0 {
                 warn!("`sched_setscheduler` return {}", rt);
@@ -250,20 +259,32 @@ impl itimerspec_t {
     }
 }
 
-extern {
+extern "C" {
     //
     fn sched_get_priority_max(_policy: c_int) -> c_int;
     fn sched_get_priority_min(_policy: c_int) -> c_int;
-    fn sched_setscheduler(_pid_t: c_int, _policy: c_int, _sched_param_t: *mut sched_param_t) -> c_int;
+    fn sched_setscheduler(_pid_t: c_int,
+                          _policy: c_int,
+                          _sched_param_t: *mut sched_param_t)
+                          -> c_int;
     fn pthread_attr_init(_pthread_attr: *mut pthread_attr_t) -> c_int;
-    fn pthread_attr_setschedparam(_pthread_attr: *mut pthread_attr_t, _sched_param_t: *const sched_param_t) -> c_int;
+    fn pthread_attr_setschedparam(_pthread_attr: *mut pthread_attr_t,
+                                  _sched_param_t: *const sched_param_t)
+                                  -> c_int;
     //
     fn clock_gettime(_clock_id: c_int, _time_spec: *mut timespec_t) -> c_int;
     fn clock_settime(_clock_id: c_int, _time_spec: *const timespec_t) -> c_int;
     //
-    fn timer_create(_clock_id: c_int, _sigevent_t: *mut sigevent_t, _timer_t: *mut TimerId) -> c_int;
+    fn timer_create(_clock_id: c_int,
+                    _sigevent_t: *mut sigevent_t,
+                    _timer_t: *mut TimerId)
+                    -> c_int;
     fn timer_delete(_timer_t: TimerId) -> c_int;
-    fn timer_settime(_timer_t: TimerId, _flags: c_int, _itimerspec: *const itimerspec_t, _ovalue: *mut itimerspec_t) -> c_int;
+    fn timer_settime(_timer_t: TimerId,
+                     _flags: c_int,
+                     _itimerspec: *const itimerspec_t,
+                     _ovalue: *mut itimerspec_t)
+                     -> c_int;
     fn timer_getoverrun(_timer_t: TimerId) -> c_int;
     //
     fn adjtime(delta: *mut timeval, olddelta: *mut timeval) -> c_int;
@@ -271,9 +292,7 @@ extern {
 
 pub fn set_os_real_time(sec: i64, nsec: i64) -> Result<()> {
     let spec = timespec_t::new(sec as c_long, nsec as c_long);
-    let rt = unsafe {
-        clock_settime(CLOCK_REALTIME, &spec)
-    };
+    let rt = unsafe { clock_settime(CLOCK_REALTIME, &spec) };
     if rt != 0 {
         Err(Error::last_os_error())
     } else {
@@ -283,9 +302,7 @@ pub fn set_os_real_time(sec: i64, nsec: i64) -> Result<()> {
 
 pub fn get_os_real_time() -> Result<(i64, i64)> {
     let mut spec = timespec_t::new(0, 0);
-    let rt = unsafe {
-        clock_gettime(CLOCK_REALTIME, &mut spec)
-    };
+    let rt = unsafe { clock_gettime(CLOCK_REALTIME, &mut spec) };
     if rt != 0 {
         Err(Error::last_os_error())
     } else {
@@ -298,11 +315,11 @@ struct timeval {
     tv_sec: i32,
     tv_usec: i32,
 }
-// adjust system time with ms. return old timeval(ms)
-pub fn adjust_os_time(duration_ms: i32) -> i32 {
+// adjust system time with (s, us). return old timeval(s, us)
+pub fn adjust_os_time(s: i32, us: i32) -> (i32, i32) {
     let mut ti = timeval {
-        tv_sec: 0,
-        tv_usec: duration_ms * 1000,
+        tv_sec: s,
+        tv_usec: us,
     };
 
     let mut old = timeval {
@@ -312,7 +329,7 @@ pub fn adjust_os_time(duration_ms: i32) -> i32 {
 
     unsafe {
         let rt = adjtime(&mut ti, &mut old);
-        return rt;
+        return (old.tv_sec, old.tv_usec);
     }
 }
 
